a326b317e9ef68b179c87748492c1e5ee129db63
Juan Guerra <juanguerra97@protonmail.com>
2020-03-05 09:40:30 -0600

    creacion component Juego

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JuegoComponent } from './juego/juego.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegoComponent
   ],
   imports: [
     BrowserModule,
